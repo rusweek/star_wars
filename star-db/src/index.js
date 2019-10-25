606099ed9c0e3cb9d3adabd122deb630f703f67d
rusweek <rusweek@gmail.com>
2019-10-25 21:59:44 +0700

    done what i wanted

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,2 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/app";
 
-console.log("hello");
+ReactDOM.render(<App />, document.getElementById('root'));
