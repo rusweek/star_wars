fb9f1bcea5fa769fe37f55084ca4a0089b695581
rusweek <rusweek@gmail.com>
2019-11-11 01:12:15 +0700

    убрана проблема с мерцанием картинки-заглушки, при ошибки загрузке основной картинки

--- star-db/src/components/image-component/image-component.js
+++ star-db/src/components/image-component/image-component.js
@@ -3,20 +3,7 @@ import icon from './death-star.png';
 class ImageComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            imageStatus: "loading",
-            error: false
-        };
-    }
 
-
-
-    handleImageLoaded() {
-        this.setState({ imageStatus: "loaded", error: false });
-    }
-
-    handleImageError() {
-        this.setState({ imageStatus: "failed to load", error: true });
     }
 
     shouldComponentUpdate(nextProps) {
@@ -25,16 +12,17 @@ class ImageComponent extends React.Component {
         }
     }
 
+    addDefaultSrc(ev){
+        ev.target.src = icon;
+    }
+
 
     render() {
         return (
-            <div style={ {width:'150px', height: '150px', }}>
-                <img className="planet-image" alt="img"
-                    src={this.state.error ? icon : this.props.imageUrl}
-                    onLoad={this.handleImageLoaded.bind(this)}
-                    onError={this.handleImageError.bind(this)}
-                />
-
+            <div >
+                <img style={ {width:'150px', height: '150px', }}
+                     onError={this.addDefaultSrc} className="img-responsive"
+                     src={this.props.imageUrl} alt="img"/>
             </div>
         );
     }
